b356aa32fec39798910d37396cff021083d968af
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-19 19:07:27 +0100

    unknown change

--- script.sh
+++ script.sh
@@ -5,7 +5,6 @@ if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u}
 then
     git pull
     docker stop robocist/notion-buddy:latest
-    echo "${TOKEN}"
     docker build . -t robocist/notion-buddy:latest --build-arg "${TOKEN}"
     docker run robocist/notion-buddy:latest
 fi
