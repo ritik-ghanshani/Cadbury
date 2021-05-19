1366ca7088562db2f554f70cbebe02eddd878310
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-20 03:04:15 +0530

    added hello command

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+from discord_slash import SlashCommand, SlashContext
 
 load_dotenv()
 #TOKEN = os.getenv('DISCORD_TOKEN')
 TOKEN = os.environ['DISCORD_TOKEN']
 
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
 client = discord.Client()
+slash = SlashCommand(bot, sync_commands=True)
 
 @client.event
 async def on_ready():
-    await client.Guild.text_channels[-1].send(f'{client.user.name} has connected to Discord!')
-    #print(f'{client.user.name} has connected to Discord!')
+    #await client.guilds[0].text_channels[-1].send(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
 @client.event
 async def on_member_join(member):
@@ -20,7 +25,14 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
+@bot.command(name="hello")
+async def message(message):
+    await message.channel.send(" ".join(message.message.content.split(" ")[1:]))
 
-client.run(TOKEN)
+@slash.slash(name="hello")
+async def _hello(ctx: SlashContext):
+    await ctx.send(content="never gonna let you down, never gonna run around")
 
-#
+
+bot.run(TOKEN)
+#client.run(TOKEN)
