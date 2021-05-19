c3d29fe41979ba365cb70a9b9d6a1a890536e22b
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-20 01:14:54 +0530

    sends message when connected

--- bot.py
+++ bot.py
@@ -10,7 +10,16 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    await client.Guild.text_channels[-1].send(f'{client.user.name} has connected to Discord!')
+    #print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
 
 client.run(TOKEN)
 
