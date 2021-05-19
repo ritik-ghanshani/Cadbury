79f52878e473517ec70e27689c5c3343f1dc635e
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-20 00:27:19 +0530

    fixed script

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ cd ~/Projects/NotionBuddy
 if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \sed 's/\// /g') | cut -f1) ]]
 then
     git pull
-    docker ps -a -q
+    docker stop $(docker ps -a -q)
     docker build . -t robocist/notion-buddy:latest --build-arg "${TOKEN}"
     docker run robocist/notion-buddy:latest
 fi
