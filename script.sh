10c33b98aff741d7a33ad05ae89d4829ae556110
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-19 19:06:14 +0100

    removed token from script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#token="DISCORD_TOKEN=ODQzODg2NDc5Nzk3NTE4MzM2.YKKYhw.3M4ZxOmE4Wl-IRCxL9YBPIC7IYE"
-
 cd ~/Projects/NotionBuddy
 if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \sed 's/\// /g') | cut -f1) ]]
 then
