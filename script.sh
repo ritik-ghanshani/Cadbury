a20d566f2125428f92f7779314f669430dd50722
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-19 19:05:32 +0100

    added cron script to run on RPI

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-token="DISCORD_TOKEN=ODQzODg2NDc5Nzk3NTE4MzM2.YKKYhw.3M4ZxOmE4Wl-IRCxL9YBPIC7IYE"
+#token="DISCORD_TOKEN=ODQzODg2NDc5Nzk3NTE4MzM2.YKKYhw.3M4ZxOmE4Wl-IRCxL9YBPIC7IYE"
 
 cd ~/Projects/NotionBuddy
 if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \sed 's/\// /g') | cut -f1) ]]
 then
     git pull
     docker stop robocist/notion-buddy:latest
-    docker build . -t robocist/notion-buddy:latest --build-arg "$token"
+    echo "${TOKEN}"
+    docker build . -t robocist/notion-buddy:latest --build-arg "${TOKEN}"
     docker run robocist/notion-buddy:latest
 fi
