cf3474c71419b9e91823b9abf21736a6911fa682
Ritik Ghanshani <ritik.ghanshani@outlook.com>
2021-05-20 06:15:06 +0530

    fixed tts

--- bot.py
+++ bot.py
@@ -14,13 +14,11 @@ slash = SlashCommand(bot, sync_commands=True)
 @bot.command(name="hello", help="just echoes back what you said")
 async def message(ctx):
     print(f'{ctx.message.author.name} said {" ".join(ctx.message.content.split(" ")[1:])}', flush=True)
-    await ctx.channel.send(" ".join(ctx.message.content.split(" ")[1:]))
-    await ctx.channel.send(content=ctx.message.attachments[0].proxy_url, tts=True)
+    await ctx.channel.send(" ".join(ctx.message.content.split(" ")[1:]), tts=True)
 
 @slash.slash(name="hello")
 async def _hello(ctx: SlashContext):
     print(f'{ctx.message.author.name} said {" ".join(ctx.message.content.split(" ")[1:])}', flush=True)
-    await ctx.channel.send(" ".join(ctx.message.content.split(" ")[1:]))
-    await ctx.channel.send(content=ctx.message.attachments[0].proxy_url, tts=True)
+    await ctx.channel.send(" ".join(ctx.message.content.split(" ")[1:]), tts=True)
 
 bot.run(TOKEN)
