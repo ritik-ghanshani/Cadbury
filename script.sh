c51d8bce8f13402d5449ea909aab18d916d5811c
Ritik S Ghanshani <ritik.ghanshani@outlook.com>
2021-06-03 00:24:31 +0530

    fixed script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@ if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u}
 then
     git pull
     docker stop $(docker ps -a -q)
-    docker build . -t robocist/cadbury:latest "${TOKEN}"
+    docker build -t robocist/cadbury:latest --build-arg "${TOKEN}" --build-arg "${HOST}" --build-arg "${USERNAME}" --build-arg "${PASSWORD}" --build-arg "${DB_NAME}" --build-arg "${PORT}" .
     docker run robocist/cadbury:latest
 fi
